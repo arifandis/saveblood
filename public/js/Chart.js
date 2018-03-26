562e2dc17c3cf4d0de83cf4a451dc2e9dfb1d733
arifandis <arifandiswinata@gmail.com>
2018-03-26 22:20:38 +0700

    versi 1.0

---
+++ public/js/Chart.js
@@ -0,0 +1,52 @@
+// var lineChart = new Chart(speedCanvas, {
+//     type: 'line',
+//     data: speedData,
+//     options: chartOptions
+// });
+var densityCanvas = document.getElementById("densityChart");
+
+Chart.defaults.global.defaultFontFamily = "Lato";
+Chart.defaults.global.defaultFontSize = 18;
+
+var densityData = {
+  label: 'Density of Planets (kg/m3)',
+  data: [5427, 5243, 5514, 3933],
+  backgroundColor: [
+    'rgba(0, 99, 132, 0.6)',
+    'rgba(30, 99, 132, 0.6)',
+    'rgba(60, 99, 132, 0.6)',
+    'rgba(90, 99, 132, 0.6)',
+    'rgba(120, 99, 132, 0.6)'
+  ],
+  borderColor: [
+    'rgba(0, 99, 132, 1)',
+    'rgba(30, 99, 132, 1)',
+    'rgba(60, 99, 132, 1)',
+    'rgba(90, 99, 132, 1)',
+    'rgba(120, 99, 132, 1)'
+  ],
+  borderWidth: 2,
+  hoverBorderWidth: 0
+};
+
+var chartOptions = {
+  scales: {
+    yAxes: [{
+      barPercentage: 0.5
+    }]
+  },
+  elements: {
+    rectangle: {
+      borderSkipped: 'left',
+    }
+  }
+};
+
+var barChart = new Chart(densityCanvas, {
+  type: 'horizontalBar',
+  data: {
+    labels: ["Mercury", "Venus", "Earth", "Mars"],
+    datasets: [densityData],
+  },
+  options: chartOptions
+});
